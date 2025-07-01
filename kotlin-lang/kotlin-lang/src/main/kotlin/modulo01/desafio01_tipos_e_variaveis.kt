80b186e5f34f7a6b915e284c094a7bf47af7b1f0
jvic7or <jvdeoliveira2016@gmail.com>
2025-06-30 23:57:00 -0300

    modulo01: adiciona desafio01 com tipos e variáveis
    Primeiro desafio do curso Kotlin. Uso de função main, declaração de variáveis e seus tipos, e uso de println.

---
+++ kotlin-lang/kotlin-lang/src/main/kotlin/modulo01/desafio01_tipos_e_variaveis.kt
@@ -0,0 +1,28 @@
+package modulo01
+
+fun main(){
+   /*
+   * Crie um programa que possa imprimir o seu nome, idade, primeira letra do nome, altura, peso e se é programador.
+   * A saída deve ser algo como o meu exemplo abaixo:
+   * Tiago Aguiar
+   * 34
+   * T
+   * 1.69
+   * 70
+   * true
+   * */
+
+    val meuNome: String = "João Victor"
+    val idade: Int = 20
+    val primeiraLetra: Char = 'J'
+    val altura: Double = 1.73
+    val peso: Int = 85
+    val programador: Boolean = true
+
+    println(meuNome)
+    println(idade)
+    println(primeiraLetra)
+    println(altura)
+    println(peso)
+    println(programador)
+}
